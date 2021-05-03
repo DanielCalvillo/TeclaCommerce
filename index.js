e3b35b316be5dd32a21bbca185f52643ffa2ece0
DanielCalvillo <danielcalvillomancilla123@gmail.com>
2021-05-03 12:50:39 -0500

    making the container for individual products along with styles

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
+// require('dotenv').config()
 
-var url = "https://api.mercadolibre.com/categories/" 
+
+// var url = process.env.URL_CATEGORIAS
+// var idCategoria = process.env.URL_CATEGORIA
+
+var url = "https://api.mercadolibre.com/categories/"
 var idCategoria = "MLM1144"
 
 
+/* --------------------- LISTA DE CATEGORÍAS ------------------- */
 var listCategorias = document.getElementById("categorias");
-
 
 async function getCategorias(url) {
     let parseJson;
-    
+
     try {
         let getCategorias = await fetch(url + idCategoria);
         parseJson = await getCategorias.json()
         return parseJson
     } catch (error) {
-        if(parseJson.length === 0){
+        if (parseJson.length === 0) {
             console.log('Error al consumir la API')
-        }else{
+        } else {
             console.log('No existen datos');
             console.log(parseJson.message);
         }
@@ -44,19 +49,29 @@ async function asignarCategorias(url) {
     }
 }
 
+/* --------------------- LISTA DE CATEGORÍAS ------------------- */
+
+
+/* --------------------- LISTA DE PRODUCTOS POR CATEGORÍA ------------------- */
+
+// let urlProductos = 'https://api.mercadolibre.com/sites/MLM/search?category=';
+// let idProductos = 'MLM1144';
+
+var listaProductos = document.getElementById("main-products");
+
 let urlProductos = 'https://api.mercadolibre.com/sites/MLM/search?category=';
-let idProductos = 'MLM1144';
+let idProductos = 'MLM151595';
 
 async function getProductos(url) {
-    let parseJson = [];
+    let parseJson;
     try {
         let resultado = await fetch(urlProductos + idProductos);
         parseJson = await resultado.json();
         return parseJson
     } catch (error) {
-        if(parseJson.length === 0){
+        if (parseJson.length === 0) {
             console.log('Error al consumir la API')
-        }else{
+        } else {
             console.log('No existen datos');
             console.log(parseJson.message);
         }
@@ -64,12 +79,34 @@ async function getProductos(url) {
 
 }
 
+
+
 async function asignarProductos(url) {
     let productos = await getProductos(url);
     let listaDeProductos = productos.results;
     console.log(listaDeProductos)
     for (let x = 0; x < 8; x++) {
-        console.log(listaDeProductos[x])
+        const producto = listaDeProductos[x];
+        console.log(producto)
+        let contenedorProducto = document.createElement('div');
+        contenedorProducto.setAttribute('class', 'product-container')
+
+        let tituloProducto = document.createElement('h2');
+        tituloProducto.textContent = producto.title
+
+        let imagenProducto = document.createElement('img');
+        imagenProducto.setAttribute('src', producto.thumbnail)
+
+        let precioProducto = document.createElement('span');
+        precioProducto.textContent = `$${producto.price}`
+
+        contenedorProducto.appendChild(tituloProducto);
+        contenedorProducto.appendChild(imagenProducto);
+        contenedorProducto.appendChild(precioProducto);
+
+        listaProductos.appendChild(contenedorProducto)
+
+
     }
 }
 
