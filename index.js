da250992c4b1a1aace76a3d0a8269add54cd3f90
SandyJus <sandy.jusepe@gmail.com>
2021-05-03 14:47:22 -0700

    cambios con control de errores

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 
-var url = "https://api.mercadolibre.com/categories/" //POR QUÉ NO LET?
-let idCategoria = "MLM1144"
+var url = "https://api.mercadolibre.com/categories/" 
+var idCategoria = "MLM1144"
 
 
 var listCategorias = document.getElementById("categorias");
 
 
 async function getCategorias(url) {
-    let resultado = await fetch(url + idCategoria);
-    let parseJson = await resultado.json()
-    return parseJson
+    let parseJson;
+    
+    try {
+        let getCategorias = await fetch(url + idCategoria);
+        parseJson = await getCategorias.json()
+        return parseJson
+    } catch (error) {
+        if(parseJson.length === 0){
+            console.log('Error al consumir la API')
+        }else{
+            console.log('No existen datos');
+            console.log(parseJson.message);
+        }
+    }
 }
 
 async function asignarCategorias(url) {
@@ -33,15 +44,30 @@ async function asignarCategorias(url) {
     }
 }
 
+let urlProductos = 'https://api.mercadolibre.com/sites/MLM/search?category=';
+let idProductos = 'MLM1144';
+
 async function getProductos(url) {
-    let resultado = await fetch(url + "MLA/search?category=MLA1055&sold_quality=10");
-    let parseJson = await resultado.json();
-    return parseJson
+    let parseJson = [];
+    try {
+        let resultado = await fetch(urlProductos + idProductos);
+        parseJson = await resultado.json();
+        return parseJson
+    } catch (error) {
+        if(parseJson.length === 0){
+            console.log('Error al consumir la API')
+        }else{
+            console.log('No existen datos');
+            console.log(parseJson.message);
+        }
+    }
+
 }
 
 async function asignarProductos(url) {
     let productos = await getProductos(url);
-    let listaDeProductos = productos.results
+    let listaDeProductos = productos.results;
+    console.log(listaDeProductos)
     for (let x = 0; x < 8; x++) {
         console.log(listaDeProductos[x])
     }
